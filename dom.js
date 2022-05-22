24b36de28c9ec9d86d4c8f496f5cc69213adc62d
Shubham <shuubhamsharma98@gmail.com>
2022-05-22 08:27:00 +0530

    getelementbytagname

--- dom.js
+++ dom.js
@@ -37,3 +37,12 @@ for(var i = 0;i<items.length;i++){
     items[i].style.fontWeight='bold';
 }
 
+var li = document.getElementsByTagName('li');
+console.log('li');
+console.log(li[1]);
+li[2].textContent = 'Hello2';
+li[2].style.backgroundColor = 'green';
+
+for(var i = 0;i<items.length;i++){
+    items[i].style.fontWeight='bold';
+}

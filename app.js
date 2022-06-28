d9d3f18b559592081ed115db7bbda093c3db6978
Shubham <shuubhamsharma98@gmail.com>
2022-06-28 21:41:02 +0530

    requestresponse

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
  const http = require('http');
  const server = http.createServer((req,res)=>{
-console.log(req);
+    const url = req.url;
+    if(url==='/home'){
+        res.write('<html>');
+res.write('<head><title>My first page</title><head>');
+res.write('<body><h1>welcome home</h1></body>');
+res.write('</html>')
+res.end();
+    }
+    if(url==='/about'){
+        res.write('<html>');
+res.write('<head><title>My first page</title><head>');
+res.write('<body><h1>welcome to about us page</h1></body>');
+res.write('</html>')
+res.end();
+    }
+
+    if(url==='/node'){
+        res.write('<html>');
+res.write('<head><title>My first page</title><head>');
+res.write('<body><h1>welcome to my node js project</h1></body>');
+res.write('</html>')
+res.end();
+    }
+
+
+
  })
 
 

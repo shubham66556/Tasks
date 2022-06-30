7d0356238205f6bacf20a0613548444f8a82a22a
Shubham <shuubhamsharma98@gmail.com>
2022-06-30 18:31:57 +0530

    express

---
+++ server.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+app.use((req,res,next)=>{
+console.log('In the middleware!');
+next();
+})
+
+app.use((req,res,next)=>{
+console.log('In another middleware');
+res.send('<h1>hello from express</h1>');
+
+})
+const server = http.createServer(app);
+    
+
+server.listen(5000);
